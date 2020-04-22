e366abe7b3694f8bb685cac68da7d88413f27811
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-04-22 13:02:45 -0400

    adding beginning lines of code to see what the site currently looks like before going further

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function init(){
-//add your javascrip between these two lines of code
+	var button = document.getElementById('entrybutton');
+	var input = document.getElementById('entryinput');
+	var output = document.getElementById('textoutput');
  
 
 
