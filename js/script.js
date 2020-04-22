97df397f48612f3deda1c1a31b749aaea91152ee
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-04-22 13:04:25 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ function init(){
 	var button = document.getElementById('entrybutton');
 	var input = document.getElementById('entryinput');
 	var output = document.getElementById('textoutput');
+
+	button.addEventListener('click', function() {
+		output.innerHTML = input.value;
+		alert("Noah Timmer: " + input.value);
+	})
+
+}
+
  
-
-
-
-
-
-
-
 window.addEventListener('load', init);
